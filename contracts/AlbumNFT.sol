f9bff20da2de3dba482e0b6cb7336278b05e5c9e
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2023-06-16 12:15:48 -0400

    finished bool and modifier

--- contracts/AlbumNFT.sol
+++ contracts/AlbumNFT.sol
@@ -6,6 +6,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract AlbumNFT is ERC721URIStorage{
+    // state var bool for finished or unfinished album
+    bool public finished;
     // artist of the album
     address public artist;
 
@@ -25,6 +27,12 @@ contract AlbumNFT is ERC721URIStorage{
         _;
     }
 
+    // completed album modifier
+    modifier albumCompleted {
+        require(finished == false, "AlbumNFT: album production finished");
+        _;
+    }
+
     // event for new song minted
     event NewSongMinted(address indexed _artist, uint indexed _tokenId, string _tokenUri);
     
@@ -35,17 +43,19 @@ contract AlbumNFT is ERC721URIStorage{
         string name;
         SongStatus currentStatus;
     }
+
+
     // defining enum of status of song
     // unpublished is not on the album, album is on the album and removed is a deleted song not to be released
     enum SongStatus{unpublished, albumSong, removed}
 
     // usng the name as the album name that the artist wants to call the album, setting the symbol as the album symbol
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
-        artist = msg.sender;
+        artist = tx.origin;
     }
 
     // creating a song function
-    function createSong(string memory _name, string memory tokenUri) public onlyArtist returns(uint){
+    function createSong(string memory _name, string memory tokenUri) public onlyArtist albumCompleted returns(uint){
         _tokenCount++;
         uint currentTokenNum = _tokenCount;
 
@@ -65,7 +75,7 @@ contract AlbumNFT is ERC721URIStorage{
     // set track list
     // param 1 is the song number form the allSongs mapping
     // param 2 is the spot you want to add it to the album track list
-    function setSongToTrackList(uint songNum, uint trackSpot) public onlyArtist returns(bool){
+    function setSongToTrackList(uint songNum, uint trackSpot) public onlyArtist albumCompleted returns(bool){
         Song storage targetSong = _updateSongStatus(songNum, 1);
 
         trackList[trackSpot] = targetSong;
@@ -75,16 +85,15 @@ contract AlbumNFT is ERC721URIStorage{
     }
 
     // removing a song from production
-    function removeSong(uint songNum) public onlyArtist returns(Song memory){
+    function removeSong(uint songNum) public onlyArtist albumCompleted returns(Song memory){
         Song storage targetSong = _updateSongStatus(songNum, 2);
 
-        
         return targetSong;
 
     }
     
     // changing song status in allsong mapping
-    function changeSongStatus(uint songNum, uint status) public onlyArtist returns(Song memory){
+    function changeSongStatus(uint songNum, uint status) public onlyArtist albumCompleted returns(Song memory){
         Song memory targetSong = _updateSongStatus(songNum, status);
         return targetSong;
     }
@@ -126,6 +135,12 @@ contract AlbumNFT is ERC721URIStorage{
             targetSong.currentStatus = SongStatus.unpublished;
         }
         return targetSong;
+    }
+
+    // set the global state on if production is finish
+    // cannot be switched back to in production once completed
+    function finishAlbum() public onlyArtist returns(bool){
+        return finished = true;
     }
 
 
