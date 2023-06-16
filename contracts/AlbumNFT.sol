a02721263b50bd105904432d8dc4cf7bc0b1f108
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2023-06-16 11:49:15 -0400

    init commit -sc work started

---
+++ contracts/AlbumNFT.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract AlbumNFT is ERC721URIStorage{
+    // artist of the album
+    address public artist;
+
+    // token count which is the total songs minted to this collection
+    uint private _tokenCount;
+    // total number of tracks on track list
+    uint private _trackCount;
+
+    //mapping of token to song
+    mapping(uint => Song) public allSongs;
+    //mapping of songs to track list
+    mapping(uint => Song) public trackList;
+
+    // only artist modifier to restrict access to minting songs to album
+    modifier onlyArtist {
+        require(msg.sender == artist, "AlbumNFT: Only Artist can call function");
+        _;
+    }
+
+    // event for new song minted
+    event NewSongMinted(address indexed _artist, uint indexed _tokenId, string _tokenUri);
+    
+    // defining the song struct
+    struct Song{
+        uint tokenId;
+        address creator;
+        string name;
+        SongStatus currentStatus;
+    }
+    // defining enum of status of song
+    // unpublished is not on the album, album is on the album and removed is a deleted song not to be released
+    enum SongStatus{unpublished, albumSong, removed}
+
+    // usng the name as the album name that the artist wants to call the album, setting the symbol as the album symbol
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
+        artist = msg.sender;
+    }
+
+    // creating a song function
+    function createSong(string memory _name, string memory tokenUri) public onlyArtist returns(uint){
+        _tokenCount++;
+        uint currentTokenNum = _tokenCount;
+
+        _mint(msg.sender, currentTokenNum);
+        _setTokenURI(currentTokenNum, tokenUri);
+
+        Song memory newSong = Song(currentTokenNum, artist, _name, SongStatus.unpublished);
+
+        allSongs[currentTokenNum] = newSong;
+
+        emit NewSongMinted(artist, currentTokenNum, tokenUri);
+
+        return currentTokenNum;
+    }
+
+    // ALBUM MANAGEMENT FUNCTIONS
+    // set track list
+    // param 1 is the song number form the allSongs mapping
+    // param 2 is the spot you want to add it to the album track list
+    function setSongToTrackList(uint songNum, uint trackSpot) public onlyArtist returns(bool){
+        Song storage targetSong = _updateSongStatus(songNum, 1);
+
+        trackList[trackSpot] = targetSong;
+        _trackCount++;
+
+
+        return true;
+
+    }
+
+    // removing a song from production
+    function removeSong(uint songNum) public onlyArtist returns(Song memory){
+        Song storage targetSong = _updateSongStatus(songNum, 2);
+
+        if(targetSong.currentStatus == SongStatus.albumSong){
+            for(uint i = 0; i <= _trackCount; i++){
+                if(trackList[i].tokenId == targetSong.tokenId){
+                    delete trackList[i];
+                    _trackCount --;
+                }
+            }
+        }
+
+        
+
+
+        return targetSong;
+
+    }
+
+    // internal set song status helper function
+    function _updateSongStatus(uint songNum, uint songStatus) internal returns(Song storage){
+        Song storage targetSong = allSongs[songNum];
+        if(songStatus == 1){
+            targetSong.currentStatus = SongStatus.albumSong;
+
+        } else if(songStatus == 2){
+            targetSong.currentStatus = SongStatus.removed;
+        } else{
+            targetSong.currentStatus = SongStatus.unpublished;
+        }
+        return targetSong;
+    }
+
+
+    // getters
+    function getTokenCount() public view returns(uint){
+        return _tokenCount;
+    }
+
+
+    
+}
---
+++ test/AlbumNFT.test.js
@@ -0,0 +1,61 @@
+const { expect } = require("chai");
+const {ethers} = require("hardhat")
+
+const ALBUMNAME1 = "ALBUMNAME1"
+const ALBUMSYM1 = "ALBUMSYM1"
+const SONGNAME1 = "SONGNAME1"
+const SAMEPLEURI = "SAMPLEURI"
+
+describe("Album NFT Deployment", () =>{
+    let AlbumNFT1, artist, user1, user2, user3, txn, res, song1 
+    beforeEach(async () =>{
+        [artist, user1, user2, user3] = await ethers.getSigners();
+
+        const albumFactory1 = await ethers.getContractFactory("AlbumNFT")
+        AlbumNFT1 = await albumFactory1.deploy(ALBUMNAME1, ALBUMSYM1 )
+        await AlbumNFT1.deployed()
+
+        // console.log(`Album deployed to ${AlbumNFT1.address}`)
+    })
+    it("checks the album's artist was set", async () =>{
+        expect(await AlbumNFT1.artist()).to.equal(artist.address)
+    })
+    it("checks the name and symbol", async () =>{
+        expect(await AlbumNFT1.name()).to.equal(ALBUMNAME1)
+        expect(await AlbumNFT1.symbol()).to.equal(ALBUMSYM1)
+    })
+    describe("Create/manage Song Functions", () =>{
+
+        beforeEach(async () => {
+            txn = await AlbumNFT1.connect(artist).createSong(SONGNAME1, SAMEPLEURI)
+            res = await txn.wait()
+            song1 = await AlbumNFT1.allSongs(1);
+            
+
+
+        })
+        it("checks the owner, token uri, token num, song name", async () =>{
+            expect(song1.name).to.equal(SONGNAME1)
+            expect(await AlbumNFT1.ownerOf(1)).to.equal(artist.address)
+            expect(await AlbumNFT1.tokenURI(1)).to.equal(SAMEPLEURI)
+            expect(await AlbumNFT1.getTokenCount()).to.equal(1)
+        })
+        it("checks the setting song to track list", async () =>{
+            await AlbumNFT1.connect(artist).setSongToTrackList(1, 1)
+            const track1 = await AlbumNFT1.trackList(1)
+            song1 = await AlbumNFT1.allSongs(1);
+            expect(track1.name).to.equal(song1.name)
+            
+            expect(track1.currentStatus).to.equal(song1.currentStatus);
+        })
+        it("checks a song is removed from track mapping", async () =>{
+            await AlbumNFT1.connect(artist).removeSong(1);
+            const track1 = await AlbumNFT1.trackList(1)
+            song1 = await AlbumNFT1.allSongs(1);
+
+            expect(track1.name).to.equal("")
+            expect(song1.currentStatus).to.equal(2)
+        })
+    })
+
+})
